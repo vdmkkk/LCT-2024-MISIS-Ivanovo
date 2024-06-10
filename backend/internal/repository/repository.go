8caf6715e992128d4b5c0f2c01b9b2ab76ed2786
choseenonee <okaymans44@gmail.com>
2024-06-11 00:54:54 +0300

    add: building repo

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -10,3 +10,7 @@ type GeoData interface {
 	GetByUNOM(ctx context.Context, unom int) (models.GeoData, error)
 	GetByCount(ctx context.Context, count int) ([]models.GeoData, error)
 }
+
+type Building interface {
+	GetByUNOM(ctx context.Context, unom int) (models.Building, error)
+}
--- backend/internal/service/service.go
+++ backend/internal/service/service.go
@@ -9,3 +9,7 @@ type GeoData interface {
 	GetByUNOM(ctx context.Context, unom int) (models.GeoData, error)
 	GetByCount(ctx context.Context, count int) ([]models.GeoData, error)
 }
+
+type Building interface {
+	GetByUNOM(ctx context.Context, unom int) (models.Building, error)
+}
