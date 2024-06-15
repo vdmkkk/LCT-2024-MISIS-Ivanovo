0922ff07adb275bce2126b1329ad5bc92fbc5440
choseenonee <okaymans44@gmail.com>
2024-06-15 17:30:46 +0300

    add: storage service with heating_data uploader

---
+++ storage/main.py
@@ -0,0 +1,135 @@
+from fastapi import FastAPI, HTTPException, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+import pandas as pd
+from io import BytesIO
+
+import psycopg2
+from psycopg2 import sql
+
+import openpyxl
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+DB_USER = 'ivanovo'
+DB_PASSWORD = 'ivanovo'
+DB_HOST = 'localhost'
+DB_NAME = 'ivanovo'
+
+
+def get_db():
+    try:
+        conn = psycopg2.connect(
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,  # локальный хост
+            port="5432",  # стандартный порт PostgreSQL
+            database=DB_NAME
+        )
+        return conn
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="error while connecting to db" + str(e))
+
+
+def load_csv_to_db(file_path: str, table_name: str):
+    conn = None
+    try:
+        conn = get_db()
+        cursor = conn.cursor()
+        copy_sql = sql.SQL("""
+                COPY {} FROM STDIN WITH CSV HEADER DELIMITER ','
+            """).format(sql.Identifier(table_name))
+
+        with open(file_path, 'r') as f:
+            cursor.copy_expert(copy_sql, f)
+
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        raise HTTPException(status_code=500, detail="Error while importing CSV to DB: " + str(e))
+    finally:
+        if conn:
+            conn.close()
+
+
+# pip install openpyxl
+@app.post("/upload_file")
+async def create_upload_file(file: UploadFile = File(...)):
+    if file.content_type != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+        return {"error": "Invalid file type. Please upload an XLSX file."}
+
+    contents = await file.read()
+    df = pd.read_excel(BytesIO(contents))
+
+    file_name = 'output.csv'
+
+    df.to_csv(file_name, index=False, sep=',', encoding='utf-8')
+
+    df = df.rename(columns={
+        'ID УУ': 'id_uu',  # 'ID УУ': 'id_uu',
+        'ID ТУ': 'id_tu',
+        'Округ': 'district',
+        'Район': 'area',
+        'Потребители': 'consumers',
+        'Группа': 'group_type',
+        'UNOM': 'unom',
+        'Адрес': 'address',
+        'Центральное отопление(контур)': 'central_heating_contour',
+        'Марка счетчика ': 'meter_brand',
+        'Серия/Номер счетчика': 'meter_serial_number',
+        'Дата': 'date',
+        'Месяц/Год': 'month_year',
+        'Unit': 'unit',
+        'Объём поданого теплоносителя в систему ЦО': 'volume_supplied',
+        'Объём обратного теплоносителя из системы ЦО': 'volume_returned',
+        'Разница между подачей и обраткой(Подмес)': 'difference_supply_return_mix',
+        'Разница между подачей и обраткой(Утечка)': 'difference_supply_return_leak',
+        'Температура подачи': 'temperature_supply',
+        'Температура обратки': 'temperature_return',
+        'Наработка часов счётчика': 'meter_operating_hours',
+        'Расход тепловой энергии ': 'heat_energy_consumption',
+        'Ошибки': 'errors'
+    })
+
+    correct_order = [
+        'unom', 'id_uu', 'id_tu', 'district', 'area', 'consumers', 'group_type', 'address',
+        'central_heating_contour', 'meter_brand', 'meter_serial_number', 'month_year',
+        'unit', 'volume_supplied', 'volume_returned', 'difference_supply_return_mix',
+        'difference_supply_return_leak', 'temperature_supply', 'temperature_return',
+        'meter_operating_hours', 'heat_energy_consumption', 'errors'
+    ]
+
+    df = df.drop(columns=['date'])
+
+    missing_columns = set(correct_order) - set(df.columns)
+    if missing_columns:
+        raise HTTPException(status_code=400, detail=f"Отсутствуют столбцы: {missing_columns}")
+
+    df = df[correct_order]
+
+    df.to_csv(file_name, index=False, sep=',', encoding='utf-8')
+
+    load_csv_to_db(file_name, 'heating_data')
+
+    try:
+        os.remove(file_name)
+    except OSError as e:
+        raise HTTPException(status_code=500, detail=f"Error deleting file: {e}")
+
+    return {"filename": file.filename}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
