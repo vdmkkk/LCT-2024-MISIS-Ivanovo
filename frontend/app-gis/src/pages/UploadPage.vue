552ac5e9bee2a0512658682ccdf8056b7bdaa7d7
vdmkkk <soloviov.vad@gmail.com>
2024-06-16 22:18:50 +0300

    finishing touches

--- frontend/app-gis/src/pages/UploadPage.vue
+++ frontend/app-gis/src/pages/UploadPage.vue
@@ -2,6 +2,12 @@
   <q-page class="q-pa-md">
     <q-btn label="Назад" color="red-10" @click="router.push('/')" />
     <q-card class="q-pa-md" style="max-width: 600px; margin: auto">
+      <p style="font-size: 20px">
+        <b
+          >Данный раздел сайта является демонстративным ввиду нехватки времени.
+          Убедительная просьба пользоваться Swagger сервиса Storage</b
+        >
+      </p>
       <q-card-section>
         <div class="text-h6">Upload Files</div>
       </q-card-section>
@@ -27,6 +33,14 @@
             square
             outlined
             required
+            color="red-10"
+          />
+          <q-input
+            v-model="sheetName"
+            label="Название листа в таблице 5"
+            required
+            filled
+            color="red-10"
           />
           <q-file
             v-model="files.file2"
@@ -35,6 +49,7 @@
             square
             outlined
             required
+            color="red-10"
           />
           <q-file
             v-model="files.file3"
@@ -43,6 +58,7 @@
             square
             outlined
             required
+            color="red-10"
           />
           <q-file
             v-model="files.file4"
@@ -51,22 +67,7 @@
             square
             outlined
             required
-          />
-          <q-file
-            v-model="files.file5"
-            label="Агрегированная таблица"
-            filled
-            square
-            outlined
-            required
-          />
-          <q-file
-            v-model="files.file6"
-            label="Таблица ТЭЦ"
-            filled
-            square
-            outlined
-            required
+            color="red-10"
           />
           <q-btn
             type="submit"
@@ -83,7 +84,7 @@
 
 <script setup>
 import { ref } from 'vue';
-import { api } from 'src/boot/axios';
+import { storage } from 'src/boot/axios';
 import Cookies from 'js-cookie';
 import { useRouter } from 'vue-router';
 const router = useRouter();
@@ -94,10 +95,10 @@ const files = ref({
   file3: null,
   file4: null,
   file5: null,
-  file6: null,
 });
 
 const token = Cookies.get('_token');
+const sheetName = ref('');
 
 const handleSubmit = async () => {
   const formData1 = new FormData();
@@ -112,29 +113,27 @@ const handleSubmit = async () => {
   const formData4 = new FormData();
   formData4.append('file', files.value.file4);
 
-  const formData5 = new FormData();
-  formData5.append('file', files.value.file5);
-
-  const formData6 = new FormData();
-  formData6.append('file', files.value.file6);
-
   try {
-    const [response1, response2, response3, response4, response5, response6] =
-      await Promise.all([
-        api.post(`/upload_file/11/${token}`, formData1),
-        api.post(`/upload_file/5/sheet_name/${token}`, formData2),
-        api.post(`/upload_file/7/${token}`, formData3),
-        api.post(`/upload_file/aggregated/${token}`, formData4),
-        api.post('/upload_file/aggregated', formData5),
-        api.post(`/upload_file/tecs${token}`, formData6),
-      ]);
-
-    console.log('File 1 uploaded:', response1.data);
-    console.log('File 2 uploaded:', response2.data);
-    console.log('File 3 uploaded:', response3.data);
-    console.log('File 4 uploaded:', response4.data);
-    console.log('File 5 uploaded:', response5.data);
-    console.log('File 6 uploaded:', response6.data);
+    // const [response1, response2, response3, response4, response5, response6] =
+    //   await Promise.all([
+    //     storage.post(`/upload_file/11/${token}`, formData1, {
+    //       headers: { 'Content-Type': 'Content-Type: multipart/form-data' },
+    //     }),
+    //     storage.post(`/upload_file/5/${sheetName.value}/${token}`, formData2, {
+    //       headers: { 'Content-Type': 'Content-Type: multipart/form-data' },
+    //     }),
+    //     storage.post(`/upload_file/7/${token}`, formData3, {
+    //       headers: { 'Content-Type': 'Content-Type: multipart/form-data' },
+    //     }),
+    //     storage.post(`/upload_file/aggregated/${token}`, formData4, {
+    //       headers: { 'Content-Type': 'Content-Type: multipart/form-data' },
+    //     }),
+    //   ]);
+    // console.log('File 1 uploaded:', response1.data);
+    // console.log('File 2 uploaded:', response2.data);
+    // console.log('File 3 uploaded:', response3.data);
+    // console.log('File 4 uploaded:', response4.data);
+    // console.log('File 5 uploaded:', response5.data);
   } catch (error) {
     console.error('Error uploading files:', error);
   }
